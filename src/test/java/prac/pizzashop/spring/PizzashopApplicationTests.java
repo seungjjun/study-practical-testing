8a7aca49d49a3a807bc63e6ed4c81f2eaaaf4eaf
seungjjun <stw550@gmail.com>
2025-05-07 20:08:38 +0900

    init project

---
+++ src/test/java/prac/pizzashop/spring/PizzashopApplicationTests.java
@@ -0,0 +1,13 @@
+package prac.pizzashop.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzashopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
